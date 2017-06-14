7f9f4b581859cb0b91c41e54c1c62d489bac80cd
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-06-14 17:18:30 +0300

    tuning owl carousel work,Create some greetings

--- js/greeting/greetingSection.js
+++ js/greeting/greetingSection.js
@@ -1,15 +1,64 @@
-greeting=greeting||{};
+var greeting = greeting || {};
 
-greeting.section=function(){
-    var init=function(){
-        var greetingText=new Vue({
-        el:"greetings"
-    });
-}
-    var bindOwlcarousel=function(){
-            $(".js-owlcarousel").owlcarousel();}
-    return {
-        init()
-        ,bindOwlcarousel();
-        }
+greeting.section = (function () {
+  var greetingTexts = {
+    husband: {
+      name: "Муж"
+      , text: "поздравление от "
     }
+    , mom: {
+      name: "Мама"
+      , text: "поздравление от "
+    }
+    , pop: {
+      name: "Папа"
+      , text: "поздравление от "
+    }
+    , brother: {
+      name: "Брат"
+      , text: "поздравление от "
+    }
+  };
+
+  return {
+    init: function () {
+      this.render();
+      $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 5
+          }
+        }
+      });
+    }
+    , render: function () {
+               var greetingText=new Vue({
+                 el: "#greetings"
+                 , data: { greetingTexts:greetingTexts }
+      });
+               var news = new Vue({
+                 el: "#news"
+                 ,data: {
+                   header: "Немного о юбилярше"
+                   , horoskopesText: "Женщина-Рак загадочна, как сама Луна. Вы думали, что она практична и рациональна, а, узнав ее поближе, разглядели романтическую натуру и нежную, ранимую душу? Раньше вам казалось, что она – само спокойствие, однако рядом с вами ее настроение то и дело меняется, демонстрируя то радость, то задумчивость, то грусть? Поверьте, это совершенно нормально, просто мало кому женщина-Рак позволяет увидеть себя такой, как она есть: свой ранимый внутренний мир она тщательно скрывает от посторонних."
+                   , nameMeaning:"В имени Надежда огромный заряд терпения и ожидания чего-то хорошего. По своей звуковой энергетике оно обладает достаточной твердостью и основательностью, что в значительной степени сохраняется даже в уменьшительных формах имени – Надя, Наденька, Надюша и так далее."
+                 }
+               });
+               var history = new Vue({
+                 el: "#history"
+                 , data: { commonGreeting:"Пусть всегда в твоей жизни будут надежные и преданные люди. Пусь будет меньше зла ( а куда же совсем без него- оно укрепляет иммунитет ;-)) ). Счастья, много новых путешествий, впечатлений и достижения поставленных целей"}
+               })
+    }
+  }
+})();
+
+greeting.section.init();
