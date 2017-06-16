4ea247be914337368188d7a777dd97d1c062b40c
NCPI\Rybchenko <Rybchenko@ncpi.gov.by>
2017-06-16 12:15:56 +0300

    improve work of greetingPhotos with json file

--- js/greeting/greetingPhotos.js
+++ js/greeting/greetingPhotos.js
@@ -1,26 +1,19 @@
 var greeting = greeting || {};
 
 greeting.photos = (function () {
-    return {
-        init: function () {
-            this.render();
-        }
-        , render: function () {
-            var photos = new Vue({
-                el: "#photos"
-                , data: {
-                    photos: greeting.photos.getPhotos()
-                }
-            })
-        }
-        , getPhotos: function () {
-            var photos = [
-                { name: "", url: "images/nadia/citati_o_lete_3.jpg", isDoubleWidth: true }
-                , { name: "", url: "images/nadia/images.jpg", isDoubleWidth: false }
-                , { name: "", url: "images/nadia/images2.jpg", isDoubleWidth: false }
-                , { name: "", url: "images/nadia/images.jpg", isDoubleWidth: true }
-            ];
-            return photos;
-        }
+  return {
+    init: function () {
+      this.render();
     }
+    , render: function () {
+      $.getJSON("data/photos.json", function (data) {
+        var photos = new Vue({
+          el: "#photos"
+          , data: {
+            photos: data
+          }
+        });
+      });
+    }
+  }
 })();
